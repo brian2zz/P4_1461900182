5cbb0031a4beb78a674dc42140e0f8b6bab2e321
Febriansyah Dwi Kurnia <febriansyahdwi23@gmail.com>
2021-06-12 11:25:01 +0700

    default

---
+++ resources/views/about0182.blade.php
@@ -0,0 +1,9 @@
+@extends('layout0182')
+@section('badan')
+
+
+<h1 class="judul">Febriansyah Dwi Kurnia Wicaksono</h1>
+<h3 class="deskripsi">1461900182</h3>
+<h3 class="deskripsi">Praktikum Pengembangan Teknolgi Web</h3>
+<h3 class="deskripsi">Aktivitas 4</h3>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','App\Http\Controllers\PrakController@buku')->name('buku');
 Route::get('/user','App\Http\Controllers\PrakController@user')->name('user');
+Route::get('/about',function(){return view('about0182');});
 Route::get('/export','App\Http\Controllers\PrakController@export')->name('export');
